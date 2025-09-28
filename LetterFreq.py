179479e8c09d4d0ade0097ffb8ab741782a6576e
Marco Gatti <gatti.marc027@gmail.com>
2025-09-28 16:28:57 +0200

    working with both books

--- LetterFreq.py
+++ LetterFreq.py
@@ -4,7 +4,6 @@ import os
 import matplotlib.pyplot as plt
 
 total_letters = 0
-oldchar = "a"
 letters = {"a":0,"b":0,"c":0,"d":0,"e":0,"f":0,"g":0,"h":0,"i":0,"j":0,"k":0,"l":0,"m":0,"n":0,"o":0,"p":0,"q":0,"r":0,"s":0,"t":0,"u":0,"v":0,"w":0,"x":0,"y":0,"z":0}
 inBook = False
 nLines = 0
@@ -27,28 +26,30 @@ parser.add_argument(
 args = parser.parse_args()
 book = args.onlybook
 with open(args.file, "r", encoding="utf-8") as file:
-    while True:
-        char = file.read(1)
+    for line in file:
         if book == True:
-            if(oldchar == "\n" and char == "*"):
-                file.readline()
-                inBook = not inBook
-            oldchar = char
-        if book==False:
-            inBook = True 
-        if not char:
-            break
-        if inBook:
-            if(args.generalstats):
-                if char == "\n":
-                    nLines = nLines + 1
-                if char == " ":
-                    nWords=nWords + 1
-            if not char.isalpha() or not char.isascii():
-                continue
-            total_letters = total_letters+1
-            char = char.lower()
-            letters[char] = letters[char] + 1
+                if line.startswith("*** START OF"):
+                    inBook = True
+                    continue
+                if line.startswith("*** END OF"):
+                    inBook = False
+                    break
+        for char in line:
+            if book==False:
+                inBook = True 
+            if not char:
+                break
+            if inBook:
+                if(args.generalstats):
+                    if char == "\n":
+                         nLines = nLines + 1
+                    if char == " ":
+                        nWords=nWords + 1
+                if not char.isalpha() or not char.isascii():
+                    continue
+                total_letters = total_letters+1
+                char = char.lower()
+                letters[char] = letters[char] + 1
 relative_letters = {k: v / total_letters for k, v in letters.items()}
 end_time = time.time()
 print(f"Total elapsed time: {end_time - start_time:.4f} seconds")
